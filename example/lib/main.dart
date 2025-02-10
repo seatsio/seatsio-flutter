05f5013f8354306e930425e590c581265a3c9992
Ben Verbeken <ben@seats.io>
2025-02-10 09:46:59 +0100

    load demo chart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,8 +2,8 @@ import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
 import 'package:seatsio/seatsio.dart';
 
-const String YourWorkspaceKey = "";
-const String YourEventKey = "";
+ const String YourWorkspaceKey = "publicDemoKey";
+const String YourEventKey = "smallTheatreEvent2 ";
 
 void main() {
   runApp(MyApp());
