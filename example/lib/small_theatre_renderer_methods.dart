2eb6fb485895758847fcb6e53b025bf415dae86b
Ben Verbeken <ben@seats.io>
2025-03-27 10:51:18 +0100

    dropdown instead of button for chart methods

--- example/lib/small_theatre_renderer_methods.dart
+++ example/lib/small_theatre_renderer_methods.dart
@@ -1,16 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:seatsio/seatsio.dart';
-
 class SmallTheatreRendererMethods extends StatefulWidget {
   @override
   _SmallTheatreRendererMethodsState createState() => _SmallTheatreRendererMethodsState();
 }
 
 class _SmallTheatreRendererMethodsState extends State<SmallTheatreRendererMethods> {
-  final GlobalKey<SeatsioSeatingChartState> _chart = GlobalKey<SeatsioSeatingChartState>();
+  final GlobalKey<SeatsioSeatingChartState> _chart = GlobalKey();
 
-  _resetView() {
-    _chart.currentState?.resetView().then((_) => print("Reset view completed!"));
+  String _selectedAction = 'resetView';
+
+  final Map<String, Future<void> Function()> _actions = {};
+
+  @override
+  void initState() {
+    super.initState();
+
+    _actions.addAll({
+      // 're-render': () =>
+      'resetView': () => _chart.currentState?.resetView() ?? Future.value(),
+      // startNewSession
+      // listSelectedObjects
+      // clearSelection
+      // trySelectObjects
+      // doSelectObjects
+      // deselectObjects
+      // selectCategories
+      // deselectCategories
+      // pulse
+      // unpulse
+      // changeConfig
+      // findObject
+      // listCategories
+      // zoomToObjects
+      // zoomToSelectedObjects
+      // zoomToFilteredCategories
+      // zoomToSection
+      // getReportBySelectability
+      // setSpotlightObjects
+      // setSpotlightOnSelection
+      // clearSpotlightObjects
+
+
+    });
+  }
+
+  void _runSelectedAction() {
+    final selectedMethod = _actions[_selectedAction];
+    if (selectedMethod != null) {
+      selectedMethod().then((_) {
+        print("Action $_selectedAction completed.");
+      }).catchError((e) {
+        print("Error in $_selectedAction: $e");
+      });
+    }
   }
 
   @override
@@ -19,9 +62,26 @@ class _SmallTheatreRendererMethodsState extends State<SmallTheatreRendererMethod
       appBar: AppBar(title: Text("Small Theatre - Renderer Methods")),
       body: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: DropdownButton<String>(
+              value: _selectedAction,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedAction = newValue!;
+                });
+              },
+              items: _actions.keys
+                  .map((String action) => DropdownMenuItem<String>(
+                value: action,
+                child: Text(action),
+              ))
+                  .toList(),
+            ),
+          ),
           ElevatedButton(
-            onPressed: _resetView,
-            child: Text("ResetView"),
+            onPressed: _runSelectedAction,
+            child: Text("Run Action"),
           ),
           Expanded(
             child: SeatsioSeatingChart(
