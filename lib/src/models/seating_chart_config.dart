a9915f9820c22a57aee76adec5e3e5de0f03f287
mroloux <matti@seats.io>
2025-04-11 11:33:09 +0200

    Removed TODOs

--- lib/src/models/seating_chart_config.dart
+++ lib/src/models/seating_chart_config.dart
@@ -187,10 +187,6 @@ abstract class SeatingChartConfig implements Built<SeatingChartConfig, SeatingCh
   @BuiltValueField(serialize: false)
   Function(Floor? floor)? get onFloorChanged;
 
-  // TODO everything under Renderer Properties https://docs.seats.io/docs/renderer/chart-properties-chartselectedobjects
-
-  // TODO everything under Renderer Methods https://docs.seats.io/docs/renderer/chart-properties-chartrender
-
   SeatingChartConfig._();
 
   factory SeatingChartConfig([void Function(SeatingChartConfigBuilder)? updates]) {
